3a453f13b781542f17461e8f47ca659b6e2a19a0
Benjamin Xie <benjaminxieschool@gmail.com>
2026-09-21 16:00:12 -0400

    Testing using Scanner

---
+++ Freeteams.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Freeteams{
+  public static void main(String[] args){
+    Scanner myObj = new Scanner(System.in);
+    System.out.println("Say hello!");
+    String hello = myObj.nextLine();
+    System.out.println(hello);
+  }
+}
